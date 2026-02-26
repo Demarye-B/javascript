76150e96e27d452d95a695fa7cacec42b5eab015
DeMarye Blar <e3037110@student.dcccd.edu>
2026-02-26 13:08:46 -0600

    added code

---
+++ quiz.js
@@ -0,0 +1,37 @@
+var questions = [
+    'What do you call the intense fear of spiders?',
+    'Is a tomato a fruit or a vegetable?', 
+    'Is pineapple a valid pizza topping?'
+];
+
+var answers = [
+    'arachnophobia',
+    'fruit',
+    'yes'
+];
+
+function quiz() {
+    var score = 0;
+    for (var i = 0; i < questions.length; i++) {
+        var guess = 3;
+        while (guess > 0) {
+           var  ans = prompt(questions[i]);
+            if (ans == answers[i]){
+                if (guess == 3){
+                    score = score + 3;
+                } else if (guess == 2) {
+                    score = score + 2;
+                } else {
+                    score = score + 1;
+                }
+                guess = 0;
+            } else {
+                guess--;
+            }
+        }
+    }
+    return score;
+}
+
+var finalScore = quiz();
+document.write('<p>Your final score is ' + finalScore + '</p>');
