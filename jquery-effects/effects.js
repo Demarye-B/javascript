5f5aad10eb49253fa1b396dc6243089f4b01ad3a
DeMarye Blar <e3037110@student.dcccd.edu>
2026-04-09 14:23:18 -0500

    Added Code

--- jquery-effects/effects.js
+++ jquery-effects/effects.js
@@ -4,6 +4,9 @@ $(function(){
     $('#newsSignup').hide();
 
     $('#openclose').click(function () {
+         $("a").on("submit", function(event){
+            event.preventDefault();
+        });
         if ($('#newsSignup').is(":hidden")) {
             $('#newsSignup').show();
             $('#openclose').text("-")
