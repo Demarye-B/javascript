70dfc17172377058c498368f9e6072f8172b9ae2
DeMarye Blar <e3037110@student.dcccd.edu>
2026-04-18 10:45:28 -0500

    added code

--- jquery-dom/shopping.js
+++ jquery-dom/shopping.js
@@ -2,10 +2,15 @@ $(function() {
     let shopcount = 0
 
     $('.add').click(function() {
-        shopcount++
+        var idValue = $(this).attr('id');
+        var nameValue = $(this).attr('name');
+        shopcount++;
         if (shopcount > 0){
             $('#empty').hide();
         }
+        var cartLink = "<li class='cartItem' name='" + idvalue + "'>" + nameValue + " <span class='del'>Remove</span></li>";
+        $('#cart').append(cartLink);
+        $('#' + itemID).hide();
     });
 
 });
