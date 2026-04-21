b56d4b0b3b783324403f6f3d21aee766dfbbe72c
DeMarye Blar <e3037110@student.dcccd.edu>
2026-04-21 14:00:59 -0500

    debugging

--- jquery-images/gallery.js
+++ jquery-images/gallery.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#thumbs').find('img').hover(
         function() {
-            $(this).css({"border": "10px solid green", "box-shadow": "10px 5px 5px black"});
+            $(this).css({"border": "2px solid darkgreen", "box-shadow": "10px 5px 5px black"});
         },
         function() {
             $(this).css({"border": "none", "box-shadow": "none"})
