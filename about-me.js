7049299358d54853ba31d0a766f34623479d746e
DeMarye Blar <e3037110@student.dcccd.edu>
2026-05-06 15:01:31 -0500

    debugging

--- about-me.js
+++ about-me.js
@@ -1,10 +1,18 @@
 $(function() {
-    function NumberOfTheDay () {
-        var number = Math.floor(Math.random * 1236);
+    
+
+    function addition() {
+        var number1 = Math.floor(Math.random * 1236);
+        var number2 = Math.floor(Math.random * 1236);
+        var sum = number1 + number2;
+
+        return sum;
     };
 
+    var math= addition();
+
      document.getElementById("notd").innerHTML = 
-    "The number of the day is " + number + "."
+    "The number of the day is " + math + "."
 
     $(document).ready(function() {
         $('#title').animate({right: '1px', opacity: '1'}, "slow", "swing");
