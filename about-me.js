a258cd8f9979618b0633ce4d7cbf954fcc2bcc91
DeMarye Blar <e3037110@student.dcccd.edu>
2026-05-06 16:26:33 -0500

    debugging

--- about-me.js
+++ about-me.js
@@ -2,7 +2,7 @@ $(function() {
        
 
     function addition() {
-       var num1 = document.getElementById("1").innerHTML = 1 +
+      var num1 = document.getElementById("1").innerHTML = 1 +
 Math.floor(Math.random() * 145);
         var num2 = document.getElementById("2").innerHTML = 1 +
 Math.floor(Math.random() * 16);
@@ -10,9 +10,8 @@ num1 = parseInt(num1);
 num2 = parseInt(num2);
 var today = num1 + num2
 
-return today;
     };
-
+var today= addition();
      document.getElementById("notd").innerHTML = 
     "The number of the day is " + today + "."
 
